827c0a823abe219cb590da4267357e7529733b0f
Kayode Oyelesi <kayode.oyelesi@PMGTS0001.pmglobal.intranet>
2019-05-28 17:31:00 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Measure from "./measure";
+
+function App() {
+  return (
+    <div className="App">
+      <Measure />
+    </div>
+  );
+}
+
+export default App;
